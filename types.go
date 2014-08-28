fe3e12de9f8c3813d4c654b846881670e1748129
Anton Lindström <lindztr@gmail.com>
2014-08-28 21:16:00 +0200

    add upgradeStrategy

--- types.go
+++ types.go
@@ -28,24 +28,25 @@ type Response struct {
 // Application marathon application see :
 // https://github.com/mesosphere/marathon/blob/master/REST.md#apps
 type Application struct {
-	ID            string            `json:"id"`
-	Cmd           string            `json:"cmd,omitempty"`
-	Constraints   [][]string        `json:"constraints,omitempty"`
-	Container     *Container        `json:"container,omitempty"`
-	CPUs          float32           `json:"cpus,omitempty"`
-	Env           map[string]string `json:"env,omitempty"`
-	Executor      string            `json:"executor,omitempty"`
-	HealtChecks   []*HealthCheck    `json:"healtChecks,omitempty"`
-	Instances     int               `json:"instances,omitemptys"`
-	Mem           float32           `json:"mem,omitempty"`
-	Tasks         []*Task           `json:"tasks,omitempty"`
-	Ports         []int             `json:"ports,omitempty"`
-	RequirePorts  bool              `json:"requirePorts,omitempty"`
-	BackoffFactor float32           `json:"backoffFactor,omitempty"`
-	TasksRunning  int               `json:"tasksRunning,omitempty"`
-	TasksStaged   int               `json:"tasksStaged,omitempty"`
-	Uris          []string          `json:"uris,omitempty"`
-	Version       string            `json:"version,omitempty"`
+	ID              string            `json:"id"`
+	Cmd             string            `json:"cmd,omitempty"`
+	Constraints     [][]string        `json:"constraints,omitempty"`
+	Container       *Container        `json:"container,omitempty"`
+	CPUs            float32           `json:"cpus,omitempty"`
+	Env             map[string]string `json:"env,omitempty"`
+	Executor        string            `json:"executor,omitempty"`
+	HealtChecks     []*HealthCheck    `json:"healtChecks,omitempty"`
+	Instances       int               `json:"instances,omitemptys"`
+	Mem             float32           `json:"mem,omitempty"`
+	Tasks           []*Task           `json:"tasks,omitempty"`
+	Ports           []int             `json:"ports,omitempty"`
+	RequirePorts    bool              `json:"requirePorts,omitempty"`
+	BackoffFactor   float32           `json:"backoffFactor,omitempty"`
+	TasksRunning    int               `json:"tasksRunning,omitempty"`
+	TasksStaged     int               `json:"tasksStaged,omitempty"`
+	UpgradeStrategy *UpgradeStrategy  `json:"upgradeStrategy,omitempty"`
+	Uris            []string          `json:"uris,omitempty"`
+	Version         string            `json:"version,omitempty"`
 }
 
 // Container is docker parameters
@@ -65,6 +66,11 @@ type Volume struct {
 	ContainerPath string `json:"containerPath,omitempty"`
 	HostPath      string `json:"hostPath,omitempty"`
 	Mode          string `json:"mode,omitempty"`
+}
+
+// Upgrade strategy
+type UpgradeStrategy struct {
+	MinimumHealthCapacity string `json:"minimumHealthCapacity,omitempty"`
 }
 
 // HealthCheck are described here:
