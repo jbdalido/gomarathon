bae9fbe95bbab555708485c6d58f799cd292e664
Steven Borrelli <steve@borrelli.org>
2015-02-15 19:33:50 -0600

    Add Info func

--- info.go
+++ info.go
@@ -5,6 +5,18 @@ package gomarathon
  * https://mesosphere.github.io/marathon/docs/rest-api.html#server-info
  */
 
+// Get Info about the Marathon cluster
+func (c *Client) Info() (*Response, error) {
+	options := &RequestOptions{
+		Path: "info",
+	}
+	r, err := c.request(options)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
 // Leader returns the current leader
 func (c *Client) Leader() (*Response, error) {
 	options := &RequestOptions{
